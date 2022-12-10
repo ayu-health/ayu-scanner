12bec11624b391a538d55558d5328b61e074b162
vijay-ayu <vijaya.t@ayu.health>
2022-12-10 18:42:39 +0530

    code refactored

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -5,8 +5,6 @@ import {
   TouchableOpacity,
   Image,
   StyleSheet,
-  Alert,
-  Modal,
   Button,
 } from 'react-native';
 import DocumentScanner from 'react-native-document-scanner-plugin';
@@ -15,78 +13,100 @@ import {FlatGrid} from 'react-native-super-grid';
 import uuid from 'react-native-uuid';
 import Preview from '../components/Preview';
 
-
 const HomeScreen = () => {
-  const [imageList, setImageList] = useState<{id: string; uri: string}[]>(
-    [
-    ],
-  );
+  const [imageList, setImageList] = useState<{id: string; uri: string}[]>([]);
 
-  function deleteImage(id)
-  {
-    setImageList(imageList.filter(item => item.id != id))
-  }
+  const [docList, setDocList] = useState<{id: string; uri: string}[]>([
+    {
+      id: '1',
+      uri: 'file:///storage/emulated/0/Android/data/com.awesometsproject/files/Pictures/DOCUMENT_SCAN_0_20221210_1617292180228446879750690.jpg',
+    },
+    {
+      id: '2',
+      uri: 'file:///storage/emulated/0/Android/data/com.awesometsproject/files/Pictures/DOCUMENT_SCAN_0_20221210_1821145742263819844636528.jpg',
+    },
+  ]);
 
-  const [showImage, setShowImage] = useState<boolean>(false);
-  const [previewImage, setPreviewImage] = useState<{id: string; uri: string}>(null);
+  const [previewImage, setPreviewImage] = useState<{
+    id: string;
+    uri: string;
+  } | null>(null);
 
-  const [onAccept, setOnAccept] = useState<Function>(()=>{});
-  const [onDelete, setOnDelete] = useState<Function>(()=>{});
-  const [onAdd, setOnAdd] = useState<Function>(()=>{});
-  const [onRetake, setOnRetake] = useState<Function>(()=>{});
+  const [isImagePreview, setIsImagePreview] = useState(false);
 
-  const onClose = () => setPreviewImage(null)
-
-  function  closePreview () {setPreviewImage(null)}
-
-
-  const scanDocument = async () => {
+  const scanDocument = async (id?: any) => {
     const {scannedImages}: any = await DocumentScanner.scanDocument({
       letUserAdjustCrop: true,
       maxNumDocuments: 1,
     });
 
-const newId:string = uuid.v4();
+    if (scannedImages.length > 0) {
+      const newId: any = uuid.v4();
+      const newImage: {id: string; uri: string} = {
+        id: id ? id : newId,
+        uri: scannedImages[0],
+      };
 
-if(scannedImages.length>0)
-{
-  const newImage:{id: string; uri: string} = {id: newId, uri: scannedImages[0]}
-
-    setPreviewImage(newImage)
-    setImageList([...imageList, newImage])
-    setOnAccept(()=>()=>{closePreview()})
-    setOnAdd(()=>()=>{closePreview();scanDocument()})
-    setOnRetake(()=>()=>{deleteImage(newId);closePreview();scanDocument()})
-    setOnDelete(()=>()=>{deleteImage(newId);closePreview()})
-}
+      setPreviewImage(newImage);
+      if (id) {
+        setImageList(
+          imageList.map(item => {
+            if (item.id === id) {
+              return {...item, uri: scannedImages[0]};
+            } else {
+              return item;
+            }
+          }),
+        );
+      } else {
+        setImageList([...imageList, newImage]);
+      }
+    }
   };
 
-  const ImagePreview = (image: string) => {
-    
+  const ImagePreview = (image: any) => {
     setPreviewImage(image);
-    setOnAccept(()=>()=>{closePreview()})
-    setOnAdd(()=>()=>{closePreview();scanDocument()})
-    setOnRetake(()=>()=>{deleteImage(image.id);closePreview();scanDocument()})
-    setOnDelete(()=>()=>{deleteImage(image.id);closePreview()})
   };
 
   return (
     <>
       <View style={styles.container}>
-        <Button title="Scan Image" onPress={scanDocument} />
+        <Button
+          title="Scan Image"
+          onPress={() => {
+            setIsImagePreview(false);
+            scanDocument();
+          }}
+        />
       </View>
 
-
-{previewImage!= null && <Preview onAccept={onAccept} onRetake={onRetake} onDelete={onDelete} onAdd={onAdd} visible={previewImage!= null} onClose={onClose} filePath={previewImage && previewImage.uri} ></Preview>}
-                
+      {previewImage != null && (
+        <Preview
+          setPreviewImage={setPreviewImage}
+          scanDocument={scanDocument}
+          setImageList={setImageList}
+          visible={previewImage != null}
+          setDocList={setDocList}
+          docList={docList}
+          imageList={imageList}
+          fileObj={previewImage}
+          isImagePreview={isImagePreview}
+          setIsImagePreview={setIsImagePreview}
+        />
+      )}
 
       <Text>Scanned Images</Text>
       <FlatGrid
         itemDimension={100}
-        data={imageList}
+        data={docList}
         style={styles.gridView}
         renderItem={({item}) => (
-          <TouchableOpacity onPress={() => ImagePreview(item)}>
+          <TouchableOpacity
+            onPress={() => {
+              setIsImagePreview(true);
+              ImagePreview(item);
+              setImageList([item]);
+            }}>
             <Image
               resizeMode="contain"
               style={styles.itemContainer}
--- components/Preview.tsx
+++ components/Preview.tsx
@@ -1,111 +1,189 @@
-import { StyleSheet, TouchableOpacity, Modal, View, Text, Image } from "react-native";
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  View,
+  Text,
+  // Image,
+} from 'react-native';
 import ImageView from 'react-native-image-viewing';
-export default function Preview({filePath, onAccept, onRetake, onClose, onDelete,onAdd, visible})
-{
+const Preview = ({
+  setPreviewImage,
+  fileObj,
+  scanDocument,
+  setImageList,
+  imageList,
+  visible,
+  setDocList,
+  docList,
+  isImagePreview,
+  setIsImagePreview,
+}: {
+  setPreviewImage: any;
+  fileObj: {id: string; uri: string};
+  scanDocument: any;
+  setImageList: any;
+  imageList: any;
+  visible: boolean;
+  setDocList: any;
+  docList: any;
+  isImagePreview: boolean;
+  setIsImagePreview: any;
+}) => {
+  const onClose = () => {
+    setPreviewImage(null);
+    setIsImagePreview(false);
+    setImageList([]);
+    // setShowAddMore(false);
+  };
 
-    const styles = StyleSheet.create({
-        previewImageStyle: {
-          height: '90%',
-          width: '100%',
-          marginBottom: 10,
-        },
-        gridView: {
-          marginTop: 10,
-          flex: 1,
-        },
-        itemContainer: {
-          justifyContent: 'flex-start',
-          borderRadius: 5,
-          height: 150,
-        },
-        container: {
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 22,
-          width: '100%',
-        },
-        images: {
-          display: 'flex',
-          justifyContent: 'flex-start',
-          gap: '10px',
-        },
-        modalView: {
-          height: '95%',
-          margin: 20,
-          borderRadius: 20,
-          padding: 35,
-          justifyContent: 'flex-end',
-          alignItems: 'center',
-        },
-        buttonGroup:{
-            flexDirection: "row",
-            justifyContent: "space-around",
-            
-        },
-        
-        modalText: {
-          marginBottom: 15,
-          textAlign: 'center',
-          color: 'blue',
-        },
-        button: {
-          borderRadius: 20,
-          padding: 10,
-          elevation: 2,
-          backgroundColor: '#2196F3',
-        },
-        textStyle: {
-          color: 'white',
-          fontWeight: 'bold',
-          textAlign: 'center',
-        },
-      });
-      
+  const onAccept = () => closePreview();
 
-    return (
+  function closePreview() {
+    setPreviewImage(null);
+  }
 
-        <Modal
-        animationType="slide"
-        transparent={true}
-        visible={visible}
-        onRequestClose={onAccept}>
-        <View>
-          <View style={styles.modalView}>
-            {/* <Text style={styles.modalText}>Document Preview</Text> */}
-            <ImageView
-              images={[{uri: filePath}]}
-              imageIndex={0}
-              visible={visible}
-              onRequestClose={onClose}
-            />
-    <View style={styles.buttonGroup}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={onDelete}>
-              <Text style={styles.textStyle}>Delete</Text>
-            </TouchableOpacity>
+  const onDelete = (id: any) => {
+    setDocList((current: any) => current.filter((item: any) => item.id !== id));
+    closePreview();
+  };
+  const onRetake = (id: any) => {
+    // deleteImage(previewImage?.id);
+    closePreview();
+    scanDocument(id);
+  };
 
+  const onAdd = () => {
+    closePreview();
+    scanDocument();
+  };
 
-            <TouchableOpacity
-              style={styles.button}
-              onPress={onRetake}>
-              <Text style={styles.textStyle}>Retake</Text>
-            </TouchableOpacity>
+  const onDone = () => {
+    closePreview();
+    setDocList([...docList, ...imageList]);
+    setImageList([]);
+  };
 
-
-            <TouchableOpacity
-              style={styles.button}
-              onPress={onAdd}>
-              <Text style={styles.textStyle}>Add More</Text>
-            </TouchableOpacity>
-            </View>
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onAccept}>
+      <View>
+        <View style={styles.modalView}>
+          <ImageView
+            // images={imageList}
+            images={[{uri: imageList[imageList.length - 1]?.uri}]}
+            imageIndex={0}
+            visible={visible}
+            onRequestClose={onClose}
+          />
+          <View style={styles.buttonGroup}>
+            {isImagePreview && (
+              <TouchableOpacity
+                style={styles.deleteButton}
+                onPress={() => onDelete(fileObj.id)}>
+                <Text style={styles.textStyle}>Delete</Text>
+              </TouchableOpacity>
+            )}
+            {!isImagePreview && (
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => onRetake(fileObj.id)}>
+                <Text style={styles.textStyle}>Retake</Text>
+              </TouchableOpacity>
+            )}
+            {!isImagePreview && (
+              <TouchableOpacity style={styles.button} onPress={onAdd}>
+                <Text style={styles.textStyle}>Add More</Text>
+              </TouchableOpacity>
+            )}
+            {!isImagePreview && (
+              <TouchableOpacity style={styles.doneButton} onPress={onDone}>
+                <Text style={styles.textStyle}>Done</Text>
+              </TouchableOpacity>
+            )}
           </View>
         </View>
-      </Modal>
+      </View>
+    </Modal>
+  );
+};
 
+const styles = StyleSheet.create({
+  previewImageStyle: {
+    height: '90%',
+    width: '100%',
+    marginBottom: 10,
+  },
+  gridView: {
+    marginTop: 10,
+    flex: 1,
+  },
+  itemContainer: {
+    justifyContent: 'flex-start',
+    borderRadius: 5,
+    height: 150,
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    width: '100%',
+  },
+  images: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    gap: '10px',
+  },
+  modalView: {
+    zIndex: 100,
+    height: '95%',
+    margin: 20,
+    borderRadius: 20,
+    padding: 35,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    zIndex: 10000,
+  },
 
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    color: 'blue',
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: '#2196F3',
+  },
+  deleteButton: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: '#FF0000',
+  },
+  doneButton: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: '#00ff00',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
-    )
-
-}
+export default Preview;
